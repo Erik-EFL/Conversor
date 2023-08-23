6b145cfc189a224006643418842e334bbb572dc3
Erik-EFL <erik.lima.f@outlook.com>
2023-08-23 12:24:27 -0300

    fix: :bug: Fixing conversion rout
    Fixed a bug that was generating a second document

--- src/app/api/convert/route.ts
+++ src/app/api/convert/route.ts
@@ -1,15 +1,15 @@
-import { NextResponse } from 'next/server';
+import { convertDocxToHtmlWithouchImage } from '@/app/api/mammoth.utils';
 import fs from 'fs';
+import { NextResponse } from 'next/server';
 import path from 'path';
-import convertDocxToHtml from '@/app/api/mammoth.utils';
 
 export const GET = async (req: Request, res: Response) => {
   console.log("estou funcionando GET")
 }
 export const POST = async (req: Request) => {
   try {
-    const docxDirectory = 'src/assets/docx';
-    const htmlOutputDirectory = 'src/assets/html';
+    const docxDirectory = 'assets/docx';
+    const htmlOutputDirectory = 'assets/html';
 
     const docxFiles = fs.readdirSync(docxDirectory);
 
@@ -22,13 +22,20 @@ export const POST = async (req: Request) => {
     for (const docxFile of docxFiles) {
       if (docxFile.endsWith('.docx')) {
         const docxPath = path.join(docxDirectory, docxFile);
-        const htmlContent = await convertDocxToHtml(docxPath);
+        const destinationFolderName = docxFile.replace('.docx', '');
+        const destinationPath = path.join(htmlOutputDirectory, destinationFolderName);
 
-        const htmlFileName = `${docxFile.replace('.docx', '.html')}`;
-        const outputPath = path.join(htmlOutputDirectory, htmlFileName);
+        if (!fs.existsSync(destinationPath)) {
+          fs.mkdirSync(destinationPath);
+        }
+
+        const htmlContent = await convertDocxToHtmlWithouchImage(docxPath);
+
+        const htmlFileName = 'index.html';
+        const outputPath = path.join(destinationPath, htmlFileName);
         fs.writeFileSync(outputPath, htmlContent);
 
-        convertedFiles.push({ docx: docxFile, html: htmlFileName });
+        convertedFiles.push({ docx: docxFile, html: path.join(destinationFolderName, htmlFileName) });
       }
     }
 
